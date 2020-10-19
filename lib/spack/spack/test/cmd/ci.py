e3723b8e1bcefab1537602c9d0519aa50d41f759
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2020-10-19 21:07:15 +0200

    Fix gitlab_ci test
    The error was due to the update of the
    test platform.

--- lib/spack/spack/test/cmd/ci.py
+++ lib/spack/spack/test/cmd/ci.py
@@ -131,7 +131,7 @@ spack:
         compiler-agnostic: true
     mappings:
       - match:
-          - arch=test-debian6-x86_64
+          - arch=test-debian6-core2
         runner-attributes:
           tags:
             - donotcare
